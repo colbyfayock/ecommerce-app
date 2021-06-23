9090e415565ffb51f85a64155815b7cea0eec98f
colbyfayock <fayock@gmail.com>
2021-06-23 16:16:19 -0400

    fixing linting issues

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Colby's Store
+          Colby&apos;s Store
         </h1>
 
         <p className={styles.description}>
